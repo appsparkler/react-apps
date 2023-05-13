d544690d3eb49fab639556c448d3070ffadcf326
Aakash Shah <subs@appsparkler.com>
2023-05-13 21:50:42 +0530

    chore: info-item component setup

--- components/InfoItem/InfoItem.stories.tsx
+++ components/InfoItem/InfoItem.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { InfoItem } from "./InfoItem";
+import { PhoneAndroid as PhoneAndroidIcon } from "@mui/icons-material";
+import { stringResources } from "../../values/strings";
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof InfoItem> = {
   title: "Components/Info Item",
@@ -7,9 +9,14 @@ const meta: Meta<typeof InfoItem> = {
   tags: ["autodocs"],
   argTypes: {},
 };
+
 export default meta;
 type Story = StoryObj<typeof InfoItem>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const BasicExample: Story = {
-  args: {},
+  args: {
+    Icon: PhoneAndroidIcon,
+    info: stringResources.mobile,
+    borderColor: "primary.main",
+  },
 };
--- components/InfoItem/InfoItem.tsx
+++ components/InfoItem/InfoItem.tsx
@@ -1,5 +1,30 @@
-import { Box } from "@mui/material";
+import { PhoneAndroid } from "@mui/icons-material";
+import { Box, BoxProps, Typography } from "@mui/material";
 import React from "react";
-export const InfoItem = ({}) => {
-  return <Box>Info Item</Box>;
+
+interface InfoItemProps {
+  Icon: typeof PhoneAndroid;
+  info: string | undefined;
+  borderColor: BoxProps["borderColor"];
+}
+
+type InfoItemFC = React.FC<InfoItemProps>;
+
+export const InfoItem: InfoItemFC = ({ Icon, info, borderColor }) => {
+  return (
+    <Box
+      py={2}
+      pl="20%"
+      display="flex"
+      flexDirection={"row"}
+      alignItems={"center"}
+      borderTop={"2px solid"}
+      borderColor={borderColor}
+    >
+      <Icon sx={{ height: 30, width: 30 }} color="primary" />
+      <Typography variant="h6" ml={1}>
+        {info}
+      </Typography>
+    </Box>
+  );
 };
