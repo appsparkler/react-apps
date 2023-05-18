29cd298424e966075d8beafddb54d300612d87bc
Aakash Shah <subs@appsparkler.com>
2023-05-18 14:37:48 +0530

    chore: pass wrapperBoxProps (like modifier) to hero-section and info-section

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -1,12 +1,20 @@
 import { stringResources } from "@/values/strings";
-import { Box, Typography } from "@mui/material";
+import { Box, BoxProps, Typography } from "@mui/material";
 import { getGraidentTextStyle } from "./getGraidentTextStyle";
 import { AndroidSvg } from "../Icons/AndroidSvg";
 import { ReactSvg } from "../Icons/ReactSvg";
 
-export const HeroSection = () => {
+export const HeroSection: React.FC<{ wrapperBoxProps: BoxProps }> = ({
+  wrapperBoxProps = {},
+}) => {
   return (
-    <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      {...wrapperBoxProps}
+    >
       <ReactSvg aria-label="react logo" sx={{ height: 150, width: 150 }} />
       <AndroidSvg aria-label="android logo" sx={{ height: 75, width: 180 }} />
       <Typography variant="h1">{stringResources.name}</Typography>
--- components/InfoItem/InfoSection.tsx
+++ components/InfoItem/InfoSection.tsx
@@ -1,12 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, BoxProps } from "@mui/material";
 import { InfoItem } from "./InfoItem";
 import { PhoneSvgIcon } from "../Icons/PhoneSvg";
 import { stringResources } from "@/values/strings";
 import { ShareSvgIcon } from "../Icons/ShareSvg";
 import { EmailSvgIcon } from "../Icons/EmailSvg";
 
-export const InfoSection = () => (
-  <Box display={"flex"} flexDirection={"column"}>
+export const InfoSection: React.FC<{ wrapperBoxProps?: BoxProps }> = ({
+  wrapperBoxProps,
+}) => (
+  <Box display={"flex"} flexDirection={"column"} {...wrapperBoxProps}>
     <InfoItem Icon={PhoneSvgIcon} info={stringResources.mobile} />
     <InfoItem Icon={EmailSvgIcon} info={stringResources.email} />
     <InfoItem Icon={ShareSvgIcon} info={stringResources.social} />
