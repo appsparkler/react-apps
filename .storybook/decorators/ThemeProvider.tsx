878813e6b303ae81f5e5775d024450728e35dfb5
Aakash Shah <subs@appsparkler.com>
2023-05-18 11:15:14 +0530

    chore: add Lato and Montserrat fonts

--- .storybook/decorators/ThemeProvider.tsx
+++ .storybook/decorators/ThemeProvider.tsx
@@ -1,11 +1,24 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { ReactRenderer, Decorator } from "@storybook/react";
+import "@fontsource/montserrat";
+import "@fontsource/lato/900.css";
+import { Decorator } from "@storybook/react";
 import React from "react";
 
 export const ThemeProviderDecorator: Decorator = (Story) => {
   return (
     <ThemeProvider
       theme={createTheme({
+        typography: {
+          h1: {
+            fontFamily: "Montserrat",
+            fontWeight: 300,
+            fontSize: 46,
+          },
+          subtitle1: {
+            fontFamily: "Lato",
+            fontSize: 18,
+          },
+        },
         palette: {
           mode: "light",
           primary: {
