f2aca675afc7e4a24d6ee4a534237b9c13263d22
Aakash Shah <subs@appsparkler.com>
2023-05-18 16:31:08 +0530

    chore: add global styles for app

--- lib/mui/ThemeProvider.tsx
+++ lib/mui/ThemeProvider.tsx
@@ -1,12 +1,30 @@
 "use client";
-import { ThemeProvider as MUI_ThemeProvider, createTheme } from "@mui/material";
+import {
+  GlobalStyles,
+  ThemeProvider as MUI_ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { ThemeProviderProps } from "@mui/material/styles/ThemeProvider";
+import "@fontsource/montserrat/100.css";
+import "@fontsource/lato/900.css";
 
 export const ThemeProvider: React.FC<Omit<ThemeProviderProps, "theme">> = ({
   children,
 }) => (
   <MUI_ThemeProvider
     theme={createTheme({
+      typography: {
+        h1: {
+          fontFamily: "montserrat",
+          fontWeight: "100",
+          fontSize: 52,
+        },
+        subtitle2: {
+          fontFamily: "Lato",
+          fontSize: 18,
+          fontWeight: "900",
+        },
+      },
       palette: {
         mode: "dark",
         primary: {
@@ -18,6 +36,27 @@ export const ThemeProvider: React.FC<Omit<ThemeProviderProps, "theme">> = ({
       },
     })}
   >
+    <GlobalStyles
+      styles={{
+        ".special-selection::selection": {
+          color: "black",
+          background: "#3DDC84",
+        },
+        ".selection-react::selection": {
+          color: "black",
+          background: "#61dbfb",
+        },
+        ".selection-none::selection": {
+          background: "transparent",
+        },
+        html: {
+          height: "100%",
+        },
+        body: {
+          height: "100%",
+        },
+      }}
+    />
     {children}
   </MUI_ThemeProvider>
 );
